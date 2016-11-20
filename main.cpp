c73493083ae67ab328181cd65ee976ad93381190
Kevin Dennis <kevindennis@mail.usf.edu>
2016-11-19 19:01:19 -0500

    Reading in students

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
 #include <iomanip>
 
 #include "Person.h"
@@ -21,15 +22,48 @@ using namespace std;
 
 int main(void)
 {
-  //vector<Student *> students; //May want to make map with UID as key.
-  //vector<Teacher *> teachers; //Map will make it very easier to add to course after reading.
-  //vector<Course *> courses;
-  //vector<Department *> departments;
-  ifstream file("Courses.txt", ios::in);
+  map<int, Student *> students; //May want to make map with UID as key.
+  map<int, Teacher *> teachers; //Map will make it very easier to add to course after reading.
+  vector<Course *> courses;
+  vector<Department *> departments;
 
-  //Open student file
-  //file.open("Students.txt");
+
+  ifstream file("Students.txt", ios::in);
   //Read in student
+  while(true)
+  {
+    //Variables for Student constructor
+    string name = "";
+    string lvl;
+    string role;
+    string id;
+    string date;
+    string gender;
+    string program = "";
+
+    string junk; //Used to discard extra \n char at end of line
+
+    getline(file, name, '|');
+
+    if(name == "") //End of file if no name found
+      break;
+
+    getline(file, lvl, '|');
+    getline(file, role, '|');
+    getline(file, id, '|');
+    getline(file, date, '|');
+    getline(file, gender, '|');
+    getline(file, program, '|');
+    getline(file, junk); //Ignore \n
+
+    cout << name << endl;
+    cout << lvl << endl;
+    cout << role << endl;
+    cout << id << endl;
+    cout << date << endl;
+    cout << gender << endl;
+    cout << program << endl << endl;
+  }
 
   //file.close();
   //Open teachers file
@@ -37,10 +71,14 @@ int main(void)
 
   //TAs
 
-  cout << "It ran...";
-
   //Read in courses
   //file.open("Courses.txt");
+
+  /*while(!file.eof())
+  {
+    string swag;
+    file >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag >> swag;
+  }*/
 
   //Once course read in, add students by UID in file
   file.close();
